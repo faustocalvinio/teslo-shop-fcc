3dd74a87c7de0643f753e1cb3389beb0816ccbcb
Fausto Calviño <faustocalvino@outlook.com>
2024-01-08 14:44:21 +0100

    starting teslo shop project

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "@/config/fonts";
+export default function Home() {
+   return (
+      <main className="">
+         <h1>Hola Mundo</h1>
+         <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
+      </main>
+   );
+}
